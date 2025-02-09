c9b2a22d929c997d07a94df09e836963197e44bf
Fotis <fotiskapotos@gmail.com>
2025-02-09 20:44:30 +0100

    Added drag in the webapp

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ steps = st.sidebar.number_input("Number of Steps", min_value=100, max_value=5000
 dt = st.sidebar.slider("Time Step (dt)", min_value=0.001, max_value=0.01, value=0.005, step=0.001, format="%.3f")
 d = st.sidebar.number_input("Dimension (d)", min_value=2, max_value=100, value=50, step=1)
 epsilon = st.sidebar.number_input("Epsilon", min_value=1e-5, max_value=1e-2, value=1e-3, step=1e-3, format="%.4f")
+drag_coeff = st.sidebar.number_input("Epsilon", min_value=0, max_value=1, value=1e-3, step=1e-3, format="%.4f")
 
 if st.sidebar.button("Run Simulation"):
     st.write("Running simulation... This may take a moment.")
@@ -23,7 +24,7 @@ if st.sidebar.button("Run Simulation"):
     def update_progress(fraction):
         progress_bar.progress(fraction)
     
-    metric_evo = run_simulation(N_ind, N_party, steps, dt, d, epsilon, progress_callback=update_progress)
+    metric_evo = run_simulation(N_ind, N_party, steps, dt, d, epsilon, drag_coeff=drag_coeff,progress_callback=update_progress)
     st.write("Simulation complete!")
     
     fig, ax = plt.subplots(figsize=(8, 5))
